2dac1cb5f5864f3b0620ff12054f979964ee868a
Katherine Shiro <shirok@dhcp-10-102-141-145.wireless.northwestern.private>
2017-01-09 21:06:27 -0600

    adds intiailize and part of buy product function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,3 +15,38 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
 });
+
+// 
+initialize();
+
+// functions
+function initialize() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log("ITEMS FOR SALE");
+        for (var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].item_id +
+                " || Product: " + res[i].product_name +
+                " || Price: " + res[i].price);
+        }
+        buyProduct();
+    });
+}
+
+function buyProduct() {
+    inquirer.prompt([{
+        name: "product_id",
+        message: "Enter the ID of the product you'd like to buy."
+    }, {
+        name: "quantity",
+        message: "How many of the product you selected would you like to buy?"
+    }]).then(function(answers) {
+        connection.query("SELECT stock_quantity FROM products WHERE item_id = ?", [answers.product_id], function(err, res) {
+            if (answers.quantity > res[0].stock_quantity) {
+                console.log("Insufficient quantity!");
+            } else {
+                console.log("success");
+            }
+        });
+    });
+}
