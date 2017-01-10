87169c485dc7dae0729d4dd455f613064b742e1a
Katherine Shiro <shirok@Katherines-MacBook-Pro-2.local>
2017-01-10 15:49:13 -0600

    shows price

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
-//npm packages
+//dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var password = require("./password.js");
@@ -17,24 +17,29 @@ connection.connect(function(err) {
     if (err) throw err;
 });
 
-// 
+// intializes app
 initialize();
 
 // functions
 function initialize() {
+    // selects all data from products table
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
+        // logs info (ID, product, price) about all items for sale
         console.log("ITEMS FOR SALE");
         for (var i = 0; i < res.length; i++) {
             console.log("ID: " + res[i].item_id +
                 " || Product: " + res[i].product_name +
                 " || Price: " + res[i].price);
         }
+        // runs function to purchase items
         buyProduct();
     });
 }
 
+// function to purchase items
 function buyProduct() {
+    // prompts user for id and quantity of product they want to purchase
     inquirer.prompt([{
         name: "product_id",
         message: "Enter the ID of the product you'd like to buy."
@@ -42,23 +47,35 @@ function buyProduct() {
         name: "quantity",
         message: "How many of the product you selected would you like to buy?"
     }]).then(function(answers) {
+        // selects quantity of item user specified in prompt above
         connection.query("SELECT stock_quantity FROM products WHERE item_id = ?", [answers.product_id], function(err, res) {
+            // there is not enough in stock to fulfill purchase
             if (answers.quantity > res[0].stock_quantity) {
                 console.log("Insufficient quantity!");
+                // restarts function to buy items
                 buyProduct();
             } else {
+                // there is enough in stock to fulfill purchase
                 var newQty = res[0].stock_quantity - answers.quantity;
                 var prodSelected;
-                connection.query("SELECT product_name FROM products WHERE item_id = ?", [answers.product_id], function(err, res) {
+                var price;
+                // set product_name equal to prodSelected and price equal to price
+                connection.query("SELECT * FROM products WHERE item_id = ?", [answers.product_id], function(err, res) {
                     prodSelected = res[0].product_name;
+                    price = res[0].price;
                 });
+                // update table with new quantity
                 connection.query("UPDATE products SET ? WHERE ?", [{
                     stock_quantity: newQty
                 }, {
                     item_id: answers.product_id
                 }], function(err, res) {
                     if (err) throw err;
-                    console.log("Success, you've purchased " + answers.quantity + " of " + prodSelected + ".");
+                    // total price is price of 1 item * quantity purchased
+                    var total = price * answers.quantity;
+                    // let user know of their success in purchasing their items
+                    console.log("Success, you've purchased " + answers.quantity + " of " + prodSelected + " for the price of " + total + ".");
+                    // restarts function to buy items
                     buyProduct();
                 });
             }
