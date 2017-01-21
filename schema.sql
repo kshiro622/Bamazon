0cc5825868ed211262d6d66b4418daa34d3cfe8e
Katherine Shiro <katie.shiro@gmail.com>
2017-01-21 13:37:47 -0600

    fixes error in schema

--- schema.sql
+++ schema.sql
@@ -12,17 +12,17 @@ CREATE TABLE products (
 	PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Binder Clips (pkg. of 20)", "Office Supplies", "12.00", "50"),
-	   ("Coffee Mug", "Household Items", "15.00", "100"),
-	   ("100-Piece Puzzle", "Toys and Games", "9.00", "25"),
-	   ("Dasani Water Bottles (pkg. of 25)", "Groceries", "32.00", "75"),
-	   ("Dyson Vacuum Cleaner", "Household Items", "340.00", "14"),
-	   ("Pitch Perfect", "Media", "19.99", "87"),
-	   ("Starbucks K-Cups (pkg. of 20)", "Groceries", "7.50", "111"),
-	   ("Black Sharpie", "Office Supplies", "2.50", "200"),
-	   ("Scrabble", "Toys and Games", "13.50", "26"),
-	   ("End Table", "Household Items", "139.99", "19");
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Binder Clips (pkg. of 20)", "Office Supplies", "12.00", "50", "0.00"),
+	   ("Coffee Mug", "Household Items", "15.00", "100", "0.00"),
+	   ("100-Piece Puzzle", "Toys and Games", "9.00", "25", "0.00"),
+	   ("Dasani Water Bottles (pkg. of 25)", "Groceries", "32.00", "75", "0.00"),
+	   ("Dyson Vacuum Cleaner", "Household Items", "340.00", "14", "0.00"),
+	   ("Pitch Perfect", "Media", "19.99", "87", "0.00"),
+	   ("Starbucks K-Cups (pkg. of 20)", "Groceries", "7.50", "111", "0.00"),
+	   ("Black Sharpie", "Office Supplies", "2.50", "200", "0.00"),
+	   ("Scrabble", "Toys and Games", "13.50", "26", "0.00"),
+	   ("End Table", "Household Items", "139.99", "19", "0.00");
 
 CREATE TABLE departments (
 	department_id INTEGER (10) AUTO_INCREMENT NOT NULL,
